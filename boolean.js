e0b54610b6029f5117d3ddc606d420b653bfb0ed
nimely111 <samuelehnimely@gmail.com>
2022-06-29 11:16:33 +0000

    working with switch statements

--- boolean.js
+++ boolean.js
@@ -44,3 +44,29 @@
 // }
 
 // console.log(golfScore(3, 4));
+
+//switch statements
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+    case 1:
+      answer = 'alpha'
+      break;
+     case 2:
+      answer = 'beta'
+      break;
+     case 3:
+      answer = 'gamma'
+      break;
+     case 4:
+      answer = 'delta'
+      break;
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  console.log(caseInSwitch(1));
