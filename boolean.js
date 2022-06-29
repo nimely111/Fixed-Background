5642410cfa989b693867d66430f0d504c3477389
nimely111 <samuelehnimely@gmail.com>
2022-06-29 10:25:12 +0000

    working with else statement

--- boolean.js
+++ boolean.js
@@ -1,10 +1,22 @@
-function testGreaterThan(val){
-    if(val > 100){ //change this line
-        return 'over 100'; 
+// function testGreaterThan(val){
+//     if(val > 100){ //change this line
+//         return 'over 100'; 
+//     }
+//     if(val > 10){ //change this line
+//         return 'over 10';
+//     }
+//         return '10 or Under';
+// }
+// console.log(testGreaterThan(10));
+
+// else statements
+function testElse(val) {
+    let result;
+    if(val > 5){
+        result = 'Bigger than 5';
+    }else{
+        result = '5 or Smaller';
     }
-    if(val > 10){ //change this line
-        return 'over 10';
-    }
-        return '10 or Under';
+    return result;
 }
-console.log(testGreaterThan(10));
+console.log(testElse(4))
